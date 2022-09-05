65eef076e1da342b7931ac43d211fdacbb305783
郑中基 <qihao3768@gmail.com>
2022-09-05 22:41:17 +0800

    增加global.gradle抽取公共依赖内容

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -4,6 +4,33 @@ object Versions {
     const val targetSdkVersion = 33
 }
 
-object Libs {
-    const val core_ktx="androidx.core:core-ktx:1.7.0"
+object Builds {
+    const val applicationId = "com.studio.ladon"
+    const val versionCode = 1
+    const val versionName = "1.0.0"
 }
+
+object Modules {
+    const val lib_base =":lib_base"
+    const val lib_common =":lib_common"
+    const val lib_ability =":lib_ability"
+    const val module_pet =":module_pet"
+
+}
+
+object Prefix {
+    const val common = "common_"
+    const val base = "base_"
+    const val ability = "ability_"
+    const val app = "app_"
+    const val pet = "pet_"
+}
+
+object Libs {
+    const val core_ktx = "androidx.core:core-ktx:1.7.0"
+    const val arouter_api = "com.alibaba:arouter-api:1.5.2"
+    const val arouter_compiler = "com.alibaba:arouter-compiler:1.5.2"
+    const val mmkv = "com.tencent:mmkv:1.2.13"
+    const val permissionx = "com.guolindev.permissionx:permissionx:1.6.4"
+}
+
